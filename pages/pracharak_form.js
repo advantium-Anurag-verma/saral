dfd91dc9b85d70871761bcf4daf16f746f71f98d
Anurag Verma <anurag.verma@advantium.in>
2023-08-17 18:42:34 +0530

    pracharak form addresses

--- pages/pracharak_form.js
+++ pages/pracharak_form.js
@@ -1,4 +1,4 @@
-const { element, browser } = require('protractor');
+const { element, browser, by } = require('protractor');
 var EC = protractor.ExpectedConditions;
 
 let pracharak = function(){
@@ -21,7 +21,25 @@ let pracharak = function(){
     //pracharak form
     const profilePic = element(by.id('file-input'));
     let picPreviewClose = element(by.xpath('//button[text()="Close"]'));
-
+    let dob = element(by.xpath('//input[@formcontrolname="dob"]'));
+    let category =element(by.xpath('//mat-select[@id="mat-select-0"]'));
+    let caste = element(by.xpath('//input[@formcontrolname="caste"]'));
+    let subCaste = element(by.xpath('//input[@formcontrolname="sub_caste"]'));
+    let homeState = element(by.xpath('//div[@id="mat-select-value-5"]/span'));
+    let homeTown = element(by.xpath('//input[@formcontrolname="home_town"]'));
+    let landLine = element (by.xpath('//input[@formcontrolname="landline"]'));
+    let mobileNumber1 = element(by.xpath('//input[@placeholder="Mobile no"]'));
+    let addMobNum = element(by.xpath('(//div[@class="multiple-section-icons ng-star-inserted"]/mat-icon)[1]'));
+    let delMobNum = element(by.xpath('(//div[@class="multiple-section-icons ng-star-inserted"]/mat-icon)[1]/following-sibling::mat-icon'));
+    let resAdd1 = element(by.xpath('//input[@formcontrolname="res_address_1"]'));
+    let resAdd2 = element(by.xpath('//input[@formcontrolname="res_address_2"]'));
+    let resPin = element(by.xpath('//input[@formcontrolname="res_pincode"]'));
+    let offAdd1 = element(by.xpath('//input[@formcontrolname="off_address_1"]'));
+    let offAdd2 = element(by.xpath('//input[@formcontrolname="off_address_2"]'));
+    let offPin = element(by.xpath('//input[@formcontrolname="off_pincode"]'));
+    let homeTownAdd1 = element(by.xpath('//input[@formcontrolname="home_town_address_1"]'));
+    let homeTownAdd2 = element(by.xpath('//input[@formcontrolname="home_town_address_2"]'));
+    let homeTownPin = element(by.xpath('//input[@formcontrolname="home_town_pincode"]'));
 
     async function setUserName(user_name){
         await  userName.sendKeys(user_name);
@@ -80,6 +98,87 @@ let pracharak = function(){
         let radioButton = element(by.xpath("//input[@value='"+form_type+"']"));
         await browser.wait(EC.presenceOf(radioButton), 5000);
         await browser.executeScript("arguments[0].click();", radioButton);
+    };
+    this.setDob = async function(date_of_birth){
+        await  dob.sendKeys(date_of_birth);
+    };
+    this.setCategory = async function(category_name){
+        await category.click();
+        let category_type = element(by.xpath("//span[@class='mat-option-text'][text()='"+category_name+"']")).click();
+        await browser.wait(EC.presenceOf(category_type), 5000);
+        await category_type.click();
+        await browser.sleep(2000);
+    };
+    this.setCategory = async function (category_name) {
+        await category.click();
+        let categoryOption = element(by.xpath("//span[@class='mat-option-text'][text()='" + category_name + "']"));
+        await browser.wait(EC.elementToBeClickable(categoryOption), 5000);
+        await categoryOption.click();
+        await browser.sleep(2000);
+    };    
+    this.setReligion= async function(religion_type){
+        element(by.xpath('//div[@id="mat-select-value-3"]')).click();
+        let religion = element(by.xpath("//span[@class='mat-option-text'][text()='"+religion_type+"']"));
+        await browser.wait(EC.presenceOf(religion), 5000);
+        await religion.click();
+    };
+    this.setCaste = async function(casteValue){
+        await  caste.sendKeys(casteValue);
+    };
+    this.setSubCaste = async function(subCasteValue){
+        await  subCaste.sendKeys(subCasteValue);
+    };
+    this.setHomeState = async function(home_state){
+        await  homeState.click();
+        let hState = element(by.xpath("//span[@class='mat-option-text'][text()='"+home_state+"']"));
+        await browser.wait(EC.presenceOf(hState), 5000);
+        await hState.click();
+    };
+    this.setHomeTown = async function(home_town){
+        browser.actions().mouseMove(homeTown).perform();
+        await  homeTown.sendKeys(home_town);
+    };
+    this.Setlandline = async function(landline_value){
+        browser.actions().mouseMove(landLine).perform();
+        await  landLine.sendKeys(landline_value);
+    };
+    this.verifyMobNum = async function(mobile_number){
+        browser.actions().mouseMove(mobileNumber1).perform();
+        // executeScript("arguments[0]. scrollIntoView();", mobileNumber1);
+        await expect(mobileNumber1.getAttribute('value')).toBe(mobile_number);
+    };
+    this.addMobNum = async function(){
+        await addMobNum.click();
+        browser.actions().mouseMove(mobileNumber1).perform();
+        // executeScript("arguments[0]. scrollIntoView();", mobileNumber1);
+        await expect(mobileNumber1.getAttribute('value')).toBe(mobile_number);
+    };
+    this.delMobNum = async function(){
+        await delMobNum.click();
+    };
+    this.setResAdd1 = async function(res_address_1, res_pincode){
+        browser.actions().mouseMove(resAdd1).perform();
+        await resAdd1.sendKeys(res_address_1);
+        await resPin.sendKeys(res_pincode);
+    };
+    this.setResAdd2 = async function(res_address_2){
+        await resAdd2.sendKeys(res_address_2);
+    };
+    this.setOffAdd1 = async function(off_address_1, off_pincode){
+        browser.actions().mouseMove(offAdd1).perform();
+        await offAdd1.sendKeys(off_address_1);
+        await offPin.sendKeys(off_pincode);
+    };
+    this.setOffAdd2 = async function(off_address_2){
+        await offAdd2.sendKeys(off_address_2);
+    };
+    this.setHomeTownAdd1 = async function(home_town_address_1, home_town_pincode){
+        browser.actions().mouseMove(homeTownAdd1).perform();
+        await homeTownAdd1.sendKeys(home_town_address_1);
+        await homeTownPin.sendKeys(home_town_pincode);
+    };
+    this.setHomeTownAdd2 = async function(home_town_2){
+        await homeTownAdd2.sendKeys(home_town_2);
     };
 
     this.LoginCCDMS = function(){
--- testcases/pracharak form/test case/pracharak_form_fill.js
+++ testcases/pracharak form/test case/pracharak_form_fill.js
@@ -12,29 +12,29 @@ describe('File Upload Test', function (){
   const datasetNames = Object.keys(testData);
     using(testData, (data, datasetKeyIndex) => {
         const datasetName = datasetNames[datasetKeyIndex];
-    it('login_CCDMS', function(){
-      formPracharak.LoginCCDMS();
-    })
+    // it('login_CCDMS', function(){
+    //   formPracharak.LoginCCDMS();
+    // })
 
-    it('upload pracharak form', async function(){
+    // it('upload pracharak form', async function(){
 
-      await formPracharak.left_panel();
-      browser.sleep(3000);
-      await formPracharak.pracharkForm();
-      browser.sleep(3000);
-      const filePath = path.resolve(__dirname, 'C:\\Users\\anura\\OneDrive\\Documents\\Anurag\\Jarvis\\saral\\testcases\\pracharak form\\test data\\EminentUploadFormat.csv');
-      await formPracharak.fileInput(filePath);
-      browser.sleep(3000);
-      await formPracharak.submitFile();
-      browser.sleep(3000);
-      const elementToCheck = element(by.xpath('//div[@class="pusher"]/div'))
-      const timeoutMs = 5000; // Maximum time to wait for the element to be visible (in milliseconds)
-      browser.wait(EC.visibilityOf(elementToCheck), timeoutMs, 'Warning element not visible');
-      expect(elementToCheck.getText()).toBe('Success');
+    //   await formPracharak.left_panel();
+    //   browser.sleep(3000);
+    //   await formPracharak.pracharkForm();
+    //   browser.sleep(3000);
+    //   const filePath = path.resolve(__dirname, 'C:\\Users\\anura\\OneDrive\\Documents\\Anurag\\Jarvis\\saral\\testcases\\pracharak form\\test data\\EminentUploadFormat.csv');
+    //   await formPracharak.fileInput(filePath);
+    //   browser.sleep(3000);
+    //   await formPracharak.submitFile();
+    //   browser.sleep(3000);
+    //   const elementToCheck = element(by.xpath('//div[@class="pusher"]/div'))
+    //   const timeoutMs = 5000; // Maximum time to wait for the element to be visible (in milliseconds)
+    //   browser.wait(EC.visibilityOf(elementToCheck), timeoutMs, 'Warning element not visible');
+    //   expect(elementToCheck.getText()).toBe('Success');
 
-      browser.refresh();
-      await formPracharak.logout();
-    });
+    //   browser.refresh();
+    //   await formPracharak.logout();
+    // });
 
     it('login_pracharak', async function(){
       await formPracharak.LoginPracharak(data.mobile_number);
@@ -48,6 +48,36 @@ describe('File Upload Test', function (){
       await formPracharak.setProfilePic(profile_pic_path);
       await formPracharak.setFormType(data.form_type);
       browser.sleep(3000);
+      await formPracharak.setDob(data.date_of_birth);
+      browser.sleep(3000);
+      await formPracharak.setCategory(data.category_name);
+      browser.sleep(3000);
+      await formPracharak.setReligion(data.religion_type); 
+      browser.sleep(3000);
+      await formPracharak.setCaste(data.casteValue);
+      browser.sleep(3000);
+      await formPracharak.setSubCaste(data.subCasteValue);
+      browser.sleep(3000);
+      await formPracharak.setHomeState(data.home_state);
+      browser.sleep(3000);
+      await formPracharak.setHomeTown(data.home_town);
+      browser.sleep(3000);
+      await formPracharak.Setlandline(data.landline_value);
+      browser.sleep(3000);
+      await formPracharak.verifyMobNum(data.mobile_number);
+      browser.sleep(3000);
+      await formPracharak.setResAdd1(data.res_address_1, data.res_pincode);
+      browser.sleep(3000);
+      await formPracharak.setResAdd2(data.res_address_2);
+      browser.sleep(3000);
+      await formPracharak.setOffAdd1(data.off_address_1, data.off_pincode);
+      browser.sleep(3000);
+      await formPracharak.setOffAdd2(data.off_address_2);
+      browser.sleep(3000);
+      await formPracharak.setHomeTownAdd1(data.home_town_address_1, data.home_town_pincode);
+      browser.sleep(3000);
+      await formPracharak.setHomeTownAdd2(data.home_town_2);
+      browser.sleep(5000);
   });
 });
 })
