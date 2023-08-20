6b019e22531cc47c7e5f97c11c9d6d56b1fc983a
Anurag Verma <anurag.verma@advantium.in>
2023-08-20 12:37:03 +0530

    paracharak form

--- pages/pracharak_form.js
+++ pages/pracharak_form.js
@@ -40,6 +40,19 @@ let pracharak = function(){
     let homeTownAdd1 = element(by.xpath('//input[@formcontrolname="home_town_address_1"]'));
     let homeTownAdd2 = element(by.xpath('//input[@formcontrolname="home_town_address_2"]'));
     let homeTownPin = element(by.xpath('//input[@formcontrolname="home_town_pincode"]'));
+    let email = element(by.xpath('//input[@formcontrolname="email"]'));
+    let twitter = element(by.xpath('//input[@formcontrolname="twitter"]'));
+    let facebook = element(by.xpath('//input[@formcontrolname="facebook"]'));
+    let degree = element(by.xpath('//input[@formcontrolname="degree"]'));
+    let subject = element(by.xpath('//input[@formcontrolname="subject"]'));
+    let designation = element(by.xpath('//input[@formcontrolname="designation"]'));
+    let startYear = element(by.xpath('//input[@id="mat-input-42"]'));
+    let endYear = element(by.xpath('//input[@id="mat-input-43"]'));
+    let organisation = element(by.xpath('//input[@formcontrolname="organisation"]'));
+    let vibhagSambhag = element(by.xpath('//input[@formcontrolname="vibhag_sambhag"]'));
+    let state = element(by.id('mat-select-6'));
+    let district = element(by.id('mat-select-8'));
+    let mandal = element(by.id('mat-select-value-11'));
 
     async function setUserName(user_name){
         await  userName.sendKeys(user_name);
@@ -93,6 +106,7 @@ let pracharak = function(){
         browser.executeScript('arguments[0].style.display = "block";', profilePic);
         await  profilePic.sendKeys(profile_pic);
         await  picPreviewClose.click();
+        await  browser.sleep(2000);
     };
     this.setFormType = async function(form_type){
         let radioButton = element(by.xpath("//input[@value='"+form_type+"']"));
@@ -135,29 +149,32 @@ let pracharak = function(){
         await hState.click();
     };
     this.setHomeTown = async function(home_town){
-        browser.actions().mouseMove(homeTown).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', homeTown.getWebElement());
+        // browser.actions().mouseMove(homeTown).perform();
         await  homeTown.sendKeys(home_town);
     };
     this.Setlandline = async function(landline_value){
-        browser.actions().mouseMove(landLine).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', landLine.getWebElement());
+        // browser.actions().mouseMove(landLine).perform();
         await  landLine.sendKeys(landline_value);
     };
     this.verifyMobNum = async function(mobile_number){
-        browser.actions().mouseMove(mobileNumber1).perform();
-        // executeScript("arguments[0]. scrollIntoView();", mobileNumber1);
+        browser.executeScript('arguments[0].scrollIntoView()', mobileNumber1.getWebElement());
+        // browser.actions().mouseMove(mobileNumber1).perform();
         await expect(mobileNumber1.getAttribute('value')).toBe(mobile_number);
     };
     this.addMobNum = async function(){
         await addMobNum.click();
-        browser.actions().mouseMove(mobileNumber1).perform();
-        // executeScript("arguments[0]. scrollIntoView();", mobileNumber1);
+        // browser.actions().mouseMove(mobileNumber1).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', mobileNumber1.getWebElement());
         await expect(mobileNumber1.getAttribute('value')).toBe(mobile_number);
     };
     this.delMobNum = async function(){
         await delMobNum.click();
     };
     this.setResAdd1 = async function(res_address_1, res_pincode){
-        browser.actions().mouseMove(resAdd1).perform();
+        // browser.actions().mouseMove(resAdd1).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', resAdd1.getWebElement());
         await resAdd1.sendKeys(res_address_1);
         await resPin.sendKeys(res_pincode);
     };
@@ -165,7 +182,8 @@ let pracharak = function(){
         await resAdd2.sendKeys(res_address_2);
     };
     this.setOffAdd1 = async function(off_address_1, off_pincode){
-        browser.actions().mouseMove(offAdd1).perform();
+        // browser.actions().mouseMove(offAdd1).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', offAdd1.getWebElement());
         await offAdd1.sendKeys(off_address_1);
         await offPin.sendKeys(off_pincode);
     };
@@ -173,13 +191,78 @@ let pracharak = function(){
         await offAdd2.sendKeys(off_address_2);
     };
     this.setHomeTownAdd1 = async function(home_town_address_1, home_town_pincode){
-        browser.actions().mouseMove(homeTownAdd1).perform();
+        // browser.actions().mouseMove(homeTownAdd1).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', homeTownAdd1.getWebElement());
         await homeTownAdd1.sendKeys(home_town_address_1);
         await homeTownPin.sendKeys(home_town_pincode);
     };
     this.setHomeTownAdd2 = async function(home_town_2){
         await homeTownAdd2.sendKeys(home_town_2);
     };
+    this.setEmail = async function(email_value){
+        // browser.actions().mouseMove(email).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', email.getWebElement());
+        await email.sendKeys(email_value);
+    };
+    this.setTwitter = async function(twitter_value){
+        await twitter.sendKeys(twitter_value);
+    };
+    this.setFacebook = async function(facebook_value){
+        // browser.actions().mouseMove(facebook).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', facebook.getWebElement());
+        await facebook.sendKeys(facebook_value);
+    };
+    this.setDegree = async function(degree_value){
+        // browser.actions().mouseMove(degree).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', degree.getWebElement());
+        await degree.sendKeys(degree_value);
+    };
+    this.setSubject = async function(subject_value){
+        await subject.sendKeys(subject_value);
+    };
+    this.setDesignation = async function(designation_value){
+        // browser.actions().mouseMove(designation).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', designation.getWebElement());
+        await designation.sendKeys(designation_value);
+    };
+    this.setStartYear = async function(start_year){
+        await startYear.sendKeys(start_year);
+    };
+    this.setEndYear = async function(end_year){
+        await endYear.sendKeys(end_year);
+    };
+    this.setOrganisation = async function(organisation_value){
+        // browser.actions().mouseMove(organisation).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', organisation.getWebElement());
+        await organisation.sendKeys(organisation_value);
+    };
+    this.setVibhagSambhag = async function(vibhag_sambhag){
+        await vibhagSambhag.sendKeys(vibhag_sambhag);
+    };
+    this.setState = async function(state_value){
+        // browser.actions().mouseMove(state).perform();
+        browser.executeScript('arguments[0].scrollIntoView()', state.getWebElement());
+        await state.click();
+        await browser.sleep(2000);
+        let stateOption = element(by.xpath("//span[@class='mat-option-text'][text()='"+state_value+"']"));
+        await browser.wait(EC.presenceOf(stateOption), 5000);
+        await stateOption.click();
+    };
+    this.setDistrict = async function(district_value){
+        await district.click();
+        await browser.sleep(2000);
+        let districtOption = element(by.xpath("//span[@class='mat-option-text'][text()='"+ district_value+"']"));
+        await browser.wait(EC.presenceOf(districtOption), 5000);
+        await districtOption.click();
+    };
+    this.setMandal = async function(mandal_value){
+        await mandal.click();
+        await browser.sleep(2000);
+        let mandalOption = element(by.xpath("//span[@class='mat-option-text'][text()='"+ mandal_value+"']"));
+        await browser.wait(EC.presenceOf(mandalOption), 5000);
+        await mandalOption.click();
+    };
+
 
     this.LoginCCDMS = function(){
         let url = "https://saral-staging.ccdms.in/login"; 
