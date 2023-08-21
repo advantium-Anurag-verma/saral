3d16456d62eace59190d539c2a22e61cd8c81897
Anurag Verma <anurag.verma@advantium.in>
2023-08-22 00:13:46 +0530

    vanilla flow for Pracharak form

--- pages/pracharak_form.js
+++ pages/pracharak_form.js
@@ -1,6 +1,7 @@
 const { element, browser, by } = require('protractor');
 var EC = protractor.ExpectedConditions;
 
+
 let pracharak = function(){
     let userName = element(by.id('login_email'));
     let password = element(by.id('login_password'));
@@ -53,6 +54,37 @@ let pracharak = function(){
     let state = element(by.id('mat-select-6'));
     let district = element(by.id('mat-select-8'));
     let mandal = element(by.id('mat-select-value-11'));
+
+    //Father
+    let fatherName = element(by.xpath('//input[@formcontrolname="father_name"]'));
+    let fatherAge = element(by.xpath('//input[@formcontrolname="father_age"]'));
+    let fatherProfession = element(by.xpath('//input[@formcontrolname="father_profession"]'));
+    let fatherresidence = element(by.xpath('//input[@formcontrolname="father_residence"]'));
+
+    //Mother
+    let motherName = element(by.xpath('//input[@formcontrolname="mother_name"]'));
+    let motherAge = element(by.xpath('//input[@formcontrolname="mother_age"]'));
+    let motherProfession = element(by.xpath('//input[@formcontrolname="mother_profession"]'));
+    let motherresidence = element(by.xpath('//input[@formcontrolname="mother_residence"]'));
+
+    //Brother
+    let brotherName = element(by.xpath('//input[@formcontrolname="brother_name"]'));
+    let brotherAge = element(by.xpath('//input[@formcontrolname="brother_age"]'));
+    let brotherProfession = element(by.xpath('//input[@formcontrolname="brother_profession"]'));
+    let brotherresidence = element(by.xpath('//input[@formcontrolname="brother_residence"]'));
+
+    //Sister
+    let sisterName = element(by.xpath('//input[@formcontrolname="sister_name"]'));
+    let sisterAge = element(by.xpath('//input[@formcontrolname="sister_age"]'));
+    let sisterProfession = element(by.xpath('//input[@formcontrolname="sister_profession"]'));
+    let sisterresidence = element(by.xpath('//input[@formcontrolname="sister_residence"]'));
+
+    //specialInterest
+    let specialInterest = element(by.id('mat-input-29'));
+
+    //submit
+    let submitFormButton = element(by.xpath('//div[@class="col-md-12 text-center display-flex"]/button'));
+    let successMessage = element(by.xpath('//h3[text()="Your form has been updated successfully."]'));
 
     async function setUserName(user_name){
         await  userName.sendKeys(user_name);
@@ -114,15 +146,10 @@ let pracharak = function(){
         await browser.executeScript("arguments[0].click();", radioButton);
     };
     this.setDob = async function(date_of_birth){
+        await dob.click();
         await  dob.sendKeys(date_of_birth);
     };
-    this.setCategory = async function(category_name){
-        await category.click();
-        let category_type = element(by.xpath("//span[@class='mat-option-text'][text()='"+category_name+"']")).click();
-        await browser.wait(EC.presenceOf(category_type), 5000);
-        await category_type.click();
-        await browser.sleep(2000);
-    };
+    
     this.setCategory = async function (category_name) {
         await category.click();
         let categoryOption = element(by.xpath("//span[@class='mat-option-text'][text()='" + category_name + "']"));
@@ -144,28 +171,26 @@ let pracharak = function(){
     };
     this.setHomeState = async function(home_state){
         await  homeState.click();
+        browser.sleep(2000);
         let hState = element(by.xpath("//span[@class='mat-option-text'][text()='"+home_state+"']"));
         await browser.wait(EC.presenceOf(hState), 5000);
         await hState.click();
     };
     this.setHomeTown = async function(home_town){
         browser.executeScript('arguments[0].scrollIntoView()', homeTown.getWebElement());
-        // browser.actions().mouseMove(homeTown).perform();
+        await homeTown.click();
         await  homeTown.sendKeys(home_town);
     };
     this.Setlandline = async function(landline_value){
         browser.executeScript('arguments[0].scrollIntoView()', landLine.getWebElement());
-        // browser.actions().mouseMove(landLine).perform();
         await  landLine.sendKeys(landline_value);
     };
     this.verifyMobNum = async function(mobile_number){
         browser.executeScript('arguments[0].scrollIntoView()', mobileNumber1.getWebElement());
-        // browser.actions().mouseMove(mobileNumber1).perform();
         await expect(mobileNumber1.getAttribute('value')).toBe(mobile_number);
     };
     this.addMobNum = async function(){
         await addMobNum.click();
-        // browser.actions().mouseMove(mobileNumber1).perform();
         browser.executeScript('arguments[0].scrollIntoView()', mobileNumber1.getWebElement());
         await expect(mobileNumber1.getAttribute('value')).toBe(mobile_number);
     };
@@ -173,34 +198,36 @@ let pracharak = function(){
         await delMobNum.click();
     };
     this.setResAdd1 = async function(res_address_1, res_pincode){
-        // browser.actions().mouseMove(resAdd1).perform();
         browser.executeScript('arguments[0].scrollIntoView()', resAdd1.getWebElement());
+        await resAdd1.click();
         await resAdd1.sendKeys(res_address_1);
+        await resPin.click();
         await resPin.sendKeys(res_pincode);
     };
     this.setResAdd2 = async function(res_address_2){
         await resAdd2.sendKeys(res_address_2);
     };
     this.setOffAdd1 = async function(off_address_1, off_pincode){
-        // browser.actions().mouseMove(offAdd1).perform();
         browser.executeScript('arguments[0].scrollIntoView()', offAdd1.getWebElement());
+        await offAdd1.click();
         await offAdd1.sendKeys(off_address_1);
+        await offPin.click();
         await offPin.sendKeys(off_pincode);
     };
     this.setOffAdd2 = async function(off_address_2){
         await offAdd2.sendKeys(off_address_2);
     };
     this.setHomeTownAdd1 = async function(home_town_address_1, home_town_pincode){
-        // browser.actions().mouseMove(homeTownAdd1).perform();
         browser.executeScript('arguments[0].scrollIntoView()', homeTownAdd1.getWebElement());
+        await homeTownAdd1.click();
         await homeTownAdd1.sendKeys(home_town_address_1);
+        await homeTownPin.click();
         await homeTownPin.sendKeys(home_town_pincode);
     };
     this.setHomeTownAdd2 = async function(home_town_2){
         await homeTownAdd2.sendKeys(home_town_2);
     };
     this.setEmail = async function(email_value){
-        // browser.actions().mouseMove(email).perform();
         browser.executeScript('arguments[0].scrollIntoView()', email.getWebElement());
         await email.sendKeys(email_value);
     };
@@ -208,45 +235,52 @@ let pracharak = function(){
         await twitter.sendKeys(twitter_value);
     };
     this.setFacebook = async function(facebook_value){
-        // browser.actions().mouseMove(facebook).perform();
         browser.executeScript('arguments[0].scrollIntoView()', facebook.getWebElement());
         await facebook.sendKeys(facebook_value);
     };
     this.setDegree = async function(degree_value){
-        // browser.actions().mouseMove(degree).perform();
         browser.executeScript('arguments[0].scrollIntoView()', degree.getWebElement());
+        await degree.click();
         await degree.sendKeys(degree_value);
     };
     this.setSubject = async function(subject_value){
+        await subject.click();
         await subject.sendKeys(subject_value);
     };
     this.setDesignation = async function(designation_value){
-        // browser.actions().mouseMove(designation).perform();
         browser.executeScript('arguments[0].scrollIntoView()', designation.getWebElement());
+        await designation.click();
         await designation.sendKeys(designation_value);
     };
     this.setStartYear = async function(start_year){
-        await startYear.sendKeys(start_year);
+        await startYear.click();
+        browser.sleep(2000);
+        element(by.xpath("//button[@aria-label='"+start_year+"']")).click();
+        await browser.sleep(3000);
     };
     this.setEndYear = async function(end_year){
-        await endYear.sendKeys(end_year);
+        await endYear.click();
+        await browser.executeScript('arguments[0].scrollIntoView()', endYear.getWebElement());
+        await browser.sleep(2000);
+        let year = element(by.xpath("//button[@aria-label='"+end_year+"']"));
+        await year.click();
     };
     this.setOrganisation = async function(organisation_value){
-        // browser.actions().mouseMove(organisation).perform();
         browser.executeScript('arguments[0].scrollIntoView()', organisation.getWebElement());
+        await organisation.click();
         await organisation.sendKeys(organisation_value);
     };
     this.setVibhagSambhag = async function(vibhag_sambhag){
         await vibhagSambhag.sendKeys(vibhag_sambhag);
     };
     this.setState = async function(state_value){
-        // browser.actions().mouseMove(state).perform();
         browser.executeScript('arguments[0].scrollIntoView()', state.getWebElement());
         await state.click();
         await browser.sleep(2000);
         let stateOption = element(by.xpath("//span[@class='mat-option-text'][text()='"+state_value+"']"));
         await browser.wait(EC.presenceOf(stateOption), 5000);
         await stateOption.click();
+        await browser.sleep(2000);
     };
     this.setDistrict = async function(district_value){
         await district.click();
@@ -254,6 +288,7 @@ let pracharak = function(){
         let districtOption = element(by.xpath("//span[@class='mat-option-text'][text()='"+ district_value+"']"));
         await browser.wait(EC.presenceOf(districtOption), 5000);
         await districtOption.click();
+        await browser.sleep(2000);
     };
     this.setMandal = async function(mandal_value){
         await mandal.click();
@@ -261,7 +296,128 @@ let pracharak = function(){
         let mandalOption = element(by.xpath("//span[@class='mat-option-text'][text()='"+ mandal_value+"']"));
         await browser.wait(EC.presenceOf(mandalOption), 5000);
         await mandalOption.click();
+        await browser.sleep(3000);
     };
+    this.father = async function(father_name, father_residence){
+        await fatherName.click();
+        await fatherName.sendKeys(father_name);
+        await fatherresidence.click();
+        await fatherresidence.sendKeys(father_residence);
+    };
+    this.father1 = async function(father_age, father_profession, is_alive){
+        await fatherAge.sendKeys(father_age);
+        await fatherProfession.sendKeys(father_profession);
+        await fatherIsAlive.click();
+    };
+    this.mother = async function(mother_name, mother_residence){
+        await motherName.click();
+        await motherName.sendKeys(mother_name);
+        await motherresidence.click();
+        await motherresidence.sendKeys(mother_residence);
+    };
+    this.mother1 = async function(mother_age, mother_profession,is_alive){
+        let motherIsAlive = element(by.xpath("//input[@name='mat-radio-group-6'][@value='"+is_alive+"']"));
+        await motherAge.sendKeys(mother_age);
+        await motherProfession.sendKeys(mother_profession);
+        await motherIsAlive.click();
+    };
+    this.brother = async function(brother_name, brother_age, brother_profession, is_alive, brother_residence){
+        await brotherName.sendKeys(brother_name);
+        await brotherAge.sendKeys(brother_age);
+        await brotherProfession.sendKeys(brother_profession);
+        await brotherresidence.sendKeys(brother_residence);
+    };
+    this.sister = async function(sister_name, sister_age, sister_profession,is_alive, sister_residence){
+        await sisterName.sendKeys(sister_name);
+        await sisterAge.sendKeys(sister_age);
+        await sisterProfession.sendKeys(sister_profession);
+        await sisterresidence.sendKeys(sister_residence);
+    };
+    this.sInterest = async function(special_interest){
+        await specialInterest.sendKeys(special_interest);
+    };
+    this.submitForm = function(){
+        submitFormButton.click();
+        browser.sleep(3000);
+    };
+    // this.statusValue = async function(form_type){
+    //     // console.log(profile_pic.getAttribute('value'));
+    //     let radioButton = element(by.xpath("//input[@value='"+form_type+"']"));
+    //     let religion = element(by.xpath('//div[@id="mat-select-value-3"]'));
+    //     console.log("form type:",radioButton.getAttribute('value'));
+    //     console.log('Status:', radioButton.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("date of birth:",dob.getAttribute('value'));
+    //     console.log('Status:', dob.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("religion:",religion.getAttribute('value'));
+    //     console.log('Status:', religion.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("home state:",homeState.getAttribute('value'));
+    //     console.log('Status:', homeState.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("residential address:",resAdd1.getAttribute('value'));
+    //     console.log('Status:', resAdd1.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("residential pin:",resPin.getAttribute('value'));
+    //     console.log('Status:', resPin.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("office address:",offAdd1.getAttribute('value'));
+    //     console.log('Status:', offAdd1.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("office pin:",offPin.getAttribute('value'));
+    //     console.log('Status:', offPin.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("home town address:",homeTownAdd1.getAttribute('value'));
+    //     console.log('Status:', homeTownAdd1.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("home town pin:",homeTownPin.getAttribute('value'));
+    //     console.log('Status:', homeTownPin.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("degree:",degree.getAttribute('value'));
+    //     console.log('Status:', degree.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("subject:",subject.getAttribute('value'));
+    //     console.log('Status:', subject.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("designation:",designation.getAttribute('value'));
+    //     console.log('Status:', designation.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("starte year:",startYear.getAttribute('value'));
+    //     console.log('Status:', startYear.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("end year:",endYear.getAttribute('value'));
+    //     console.log('Status:', endYear.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("organisation:",organisation.getAttribute('value'));
+    //     console.log('Status:', organisation.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("state:",state.getAttribute('value'));
+    //     console.log('Status:', state.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("father's name:",fatherName.getAttribute('value'));
+    //     console.log('Status:', fatherName.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("father's residence:",fatherresidence.getAttribute('value'));
+    //     console.log('Status:', fatherresidence.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("mother's name:",motherName.getAttribute('value'));
+    //     console.log('Status:', motherName.isEnabled());
+    //     console.log("====================================================================");
+    //     console.log("mother's residence:",motherresidence.getAttribute('value'));
+    //     console.log('Status:', motherresidence.isEnabled());
+    //     console.log("====================================================================");
+    // }
+
+
+
+    this.success = async function(Success_message){
+        await browser.wait(EC.presenceOf(successMessage), 5000);
+        await expect(successMessage.getText()).toBe(Success_message);
+        console.log("====================================================================");
+        console.log("success message:",Success_message);
+        console.log("====================================================================");
+    }
 
 
     this.LoginCCDMS = function(){
--- testcases/pracharak form/test case/pracharak_form_fill.js
+++ testcases/pracharak form/test case/pracharak_form_fill.js
@@ -19,14 +19,14 @@ describe('File Upload Test', function (){
     // it('upload pracharak form', async function(){
 
     //   await formPracharak.left_panel();
-    //   browser.sleep(3000);
+    //   
     //   await formPracharak.pracharkForm();
-    //   browser.sleep(3000);
+    //   
     //   const filePath = path.resolve(__dirname, 'C:\\Users\\anura\\OneDrive\\Documents\\Anurag\\Jarvis\\saral\\testcases\\pracharak form\\test data\\EminentUploadFormat.csv');
     //   await formPracharak.fileInput(filePath);
-    //   browser.sleep(3000);
+    //   
     //   await formPracharak.submitFile();
-    //   browser.sleep(3000);
+    //   
     //   const elementToCheck = element(by.xpath('//div[@class="pusher"]/div'))
     //   const timeoutMs = 5000; // Maximum time to wait for the element to be visible (in milliseconds)
     //   browser.wait(EC.visibilityOf(elementToCheck), timeoutMs, 'Warning element not visible');
@@ -38,72 +38,132 @@ describe('File Upload Test', function (){
 
     it('login_pracharak', async function(){
       await formPracharak.LoginPracharak(data.mobile_number);
-    });
-    // it('mandatory fields check', function(){
+    })
 
-    // });
-    it('fill pracharak form', async function(){
-      const profile_pic_path = path.resolve(__dirname, "C:/Users/anura/OneDrive/Documents/Anurag/Jarvis/saral/testcases/pracharak form/test data/ProfilePicture.jfif"); 
-      browser.sleep(3000);
-      await formPracharak.setProfilePic(profile_pic_path);
+    it('mandatory fields check', async function(){
+      
       await formPracharak.setFormType(data.form_type);
-      browser.sleep(3000);
+      
       await formPracharak.setDob(data.date_of_birth);
-      browser.sleep(3000);
-      await formPracharak.setCategory(data.category_name);
-      browser.sleep(3000);
+      
       await formPracharak.setReligion(data.religion_type); 
-      browser.sleep(3000);
-      await formPracharak.setCaste(data.casteValue);
-      browser.sleep(3000);
-      await formPracharak.setSubCaste(data.subCasteValue);
-      browser.sleep(3000);
+      
       await formPracharak.setHomeState(data.home_state);
-      browser.sleep(3000);
-      await formPracharak.setHomeTown(data.home_town);
-      browser.sleep(3000);
-      await formPracharak.Setlandline(data.landline_value);
-      browser.sleep(3000);
-      await formPracharak.verifyMobNum(data.mobile_number);
-      browser.sleep(3000);
+      
       await formPracharak.setResAdd1(data.res_address_1, data.res_pincode);
-      browser.sleep(3000);
-      await formPracharak.setResAdd2(data.res_address_2);
-      browser.sleep(3000);
+      
       await formPracharak.setOffAdd1(data.off_address_1, data.off_pincode);
-      browser.sleep(3000);
-      await formPracharak.setOffAdd2(data.off_address_2);
-      browser.sleep(3000);
+      
       await formPracharak.setHomeTownAdd1(data.home_town_address_1, data.home_town_pincode);
-      browser.sleep(3000);
-      await formPracharak.setHomeTownAdd2(data.home_town_2);
-      browser.sleep(5000);
-      await formPracharak.setEmail(data.email_value);
-      browser.sleep(3000);
-      await formPracharak.setTwitter(data.twitter_value);
-      browser.sleep(3000);
-      await formPracharak.setFacebook(data.facebook_value);
-      browser.sleep(3000);
+      
       await formPracharak.setDegree(data.degree_value);
-      browser.sleep(3000);
+      
       await formPracharak.setSubject(data.subject_value);
-      browser.sleep(3000);
+      
       await formPracharak.setDesignation(data.designation_value);
-      browser.sleep(3000);
+      
       await formPracharak.setStartYear(data.start_year);
-      browser.sleep(3000);
+      
       await formPracharak.setEndYear(data.end_year);
-      browser.sleep(3000);
+      
       await formPracharak.setOrganisation(data.organisation_value);
-      browser.sleep(3000);
-      await formPracharak.setVibhagSambhag(data.vibhag_sambhag);
-      browser.sleep(3000);
+      
       await formPracharak.setState(data.state_value);
-      browser.sleep(3000);
-      await formPracharak.setDistrict(data.district_value);
-      browser.sleep(3000);
-      await formPracharak.setMandal(data.mandal_value);
-      browser.sleep(3000);
-  });
+      
+      await formPracharak.father(data.father_name, data.father_residence);
+      
+      await formPracharak.mother(data.mother_name, data.mother_residence);
+      
+      // await formPracharak.statusValue(data.form_type);
+      
+      await formPracharak.submitForm();
+      
+      await formPracharak.success(data.Success_message);
+    });
+    // it('fill pracharak form', async function(){
+      
+      // const profile_pic_path = path.resolve(__dirname, "C:/Users/anura/OneDrive/Documents/Anurag/Jarvis/saral/testcases/pracharak form/test data/ProfilePicture.jfif"); 
+      
+      // await formPracharak.setProfilePic(profile_pic_path);
+      // await formPracharak.setFormType(data.form_type);
+      
+      // await formPracharak.setDob(data.date_of_birth);
+      
+      // await formPracharak.setCategory(data.category_name);
+      
+      // await formPracharak.setReligion(data.religion_type); 
+      
+      // await formPracharak.setCaste(data.casteValue);
+      
+      // await formPracharak.setSubCaste(data.subCasteValue);
+      
+      // await formPracharak.setHomeState(data.home_state);
+      
+      // await formPracharak.setHomeTown(data.home_town);
+      
+      // await formPracharak.Setlandline(data.landline_value);
+      
+      // await formPracharak.verifyMobNum(data.mobile_number);
+      
+      // await formPracharak.setResAdd1(data.res_address_1, data.res_pincode);
+      
+      // await formPracharak.setResAdd2(data.res_address_2);
+      
+      // await formPracharak.setOffAdd1(data.off_address_1, data.off_pincode);
+      
+      // await formPracharak.setOffAdd2(data.off_address_2);
+      
+      // await formPracharak.setHomeTownAdd1(data.home_town_address_1, data.home_town_pincode);
+      
+      // await formPracharak.setHomeTownAdd2(data.home_town_2);
+      
+      // // await formPracharak.setEmail(data.email_value);
+      
+      // await formPracharak.setTwitter(data.twitter_value);
+      
+      // await formPracharak.setFacebook(data.facebook_value);
+      
+      // await formPracharak.setDegree(data.degree_value);
+      
+      // await formPracharak.setSubject(data.subject_value);
+      
+      // await formPracharak.setDesignation(data.designation_value);
+      
+      // await formPracharak.setStartYear(data.start_year);
+      
+      // await formPracharak.setEndYear(data.end_year);
+      
+      // await formPracharak.setOrganisation(data.organisation_value);
+      
+      // await formPracharak.setVibhagSambhag(data.vibhag_sambhag);
+      
+      // await formPracharak.setState(data.state_value);
+      
+      // await formPracharak.setDistrict(data.district_value);
+      
+      // await formPracharak.setMandal(data.mandal_value);
+      
+      // await formPracharak.father(data.father_name, data.father_residence);
+      
+      // await formPracharak.father1(data.father_age, data.father_profession, data.father_is_alive);
+      
+      // await formPracharak.mother(data.mother_name, data.mother_residence);
+      
+      // await formPracharak.mother1(data.mother_age, data.mother_profession, data.mother_is_alive);
+      
+      // await formPracharak.brother(data.brother_name,data.brother_age, data.brother_profession, data.brother_is_alive, data.brother_residence);
+      
+      // await formPracharak.sister(data.sister_name, data.sister_age, data.sister_profession, data.sister_is_alive, data.sister_residence);
+      
+      // await formPracharak.sInterest(data.s_interest);
+
+      // await browser.sleep(5000);
+      // await formPracharak.statusValue(data.form_type);
+      
+      // // formPracharak.submitForm();
+      // // await formPracharak.success(data.Success_message);
+
+      
+  // });
 });
 })
